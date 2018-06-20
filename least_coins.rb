a39407e3419e95948325504162b9a34257818eb8
EmilyHenken <20henkee@corjesu.org>
2018-06-20 18:10:41 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,12 @@
-#write out your code here
+# def cent_values {
+#   ":quarters" => "25"
+#   ":dimes" => "10"
+#   ":nickels" => "5"
+#   ":pennies" => "1"
+# }
+# end
 
-def least_coins(cents)
+    def make_change(amount, coins = [25, 10, 5, 1])
 
-#Code your answer here!
-
-end
+    end
+    puts make_change
